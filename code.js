b3d13044bc9de73415b7530fe52b4bcbd8282590
SVega9848 <santivediap@gmail.com>
2023-05-10 10:21:36 +0200

    add/new exercise

---
+++ code.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/59f7fc109f0e86d705000043/javascript
+// function divisibleByThree(str){
+  
+//   let fixedStr = str.split()
+
+//   let reducedNumbers = fixedStr.reduce((pVal, cVal) => pVal + cVal)
+//   let result = reducedNumbers / 3;
+
+//   return Math.floor(result) == result
+// }
